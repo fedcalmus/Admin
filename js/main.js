3a39a9c8e028fc3bcd6a26585e8825fbd844fe96
fedcalmus <fedcalmus@gmail.com>
2018-04-04 17:33:11 +0400

    Removed display block for .respMenu

---
+++ js/main.js
@@ -0,0 +1,21 @@
+        //
+        //
+        // $(function() {
+        //
+        //     var AppPlugins = {
+        //
+        //         /* ------ Select2 plugin ------ */
+        //         navBarFunction  function  () {
+        //             document.getElementsByClassName("menuItems")[0].classList.toggle("showMenuItems");
+        //         }
+        //     };
+        //
+        //     /* ------ Plugins Init ------ */
+        //     AppPlugins.navBarFunction();
+        //     AppPlugins.AccordionMenu();
+        //     // AppPlugins.mCustomScroll();
+        //
+        // });
+        // // function navBarFunction() {
+        // //     document.getElementsByClassName("menuItems")[0].classList.toggle("showMenuItems");
+        // // }
